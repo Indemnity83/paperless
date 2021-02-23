3e8034a25bc5e06fcbfeb608c33f79130e7bb743
Kyle Klaus <kklaus@indemnity83.com>
2021-02-23 08:37:25 -0800

    setup placeholder thumbnail

--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -247,7 +247,13 @@
         @foreach($files as $file)
         <div class="flex justify-between space-x-8 py-5 px-6 border-gray-300 border-b {{ $file->trashed() ? 'bg-red-50 line-through' : '' }}">
             <div class="flex-grow whitespace-nowrap overflow-hidden overflow-ellipsis">
-                <img class="inline object-cover object-top w-8 h-8 mr-3" src="/files/{{ $file->id }}/thumbnail" />
+                @if($file->thumbnail)
+                    <img class="inline object-cover object-top w-8 h-8 mr-3" src="/files/{{ $file->id }}/thumbnail" />
+                @else
+                    <svg class="inline animate-pulse w-8 h-8 mr-3 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
+                @endif
                 <a class="hover:underline" href="{{ route('files.show', $file) }}">{{ $file->name }}</a>
             </div>
             <div class="w-48 flex-none text-gray-500 hidden md:block">{{ $file->generated_at ? $file->generated_at->diffForHumans() : 'Unknown' }}</div>
